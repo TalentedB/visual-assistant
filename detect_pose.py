03084e57ef2ff51521111bf12d44b0a092374c10
Chris <81205126+TalentedB@users.noreply.github.com>
2024-02-24 04:08:38 -0800

    I dont know what I've done

---
+++ detect_pose.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+
+
+
+"""
+PoseDetector class is a wrapper around MediaPipe's Pose model.
+The class provides a method to find the pose landmarks in an image.
+The class also provides a method to draw the pose landmarks on an image.
+"""
+class PoseDetector:
+    def __init__(self, static_image_mode=False, model_complexity=1, smooth_landmarks=True, min_detection_confidence=0.5, min_tracking_confidence=0.5):
+        self.mp_pose = mp.solutions.pose
+        
+        self.pose = self.mp_pose.Pose(static_image_mode=static_image_mode, model_complexity=model_complexity, smooth_landmarks=smooth_landmarks, min_detection_confidence=min_detection_confidence, min_tracking_confidence=min_tracking_confidence)
+
+    def find_pose(self, image, draw=True, convert_to_rgb=True):
+        if convert_to_rgb:
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            
+        self.results = self.pose.process(image)
+
+        if draw:
+            if self.results.pose_landmarks:
+                image = self.draw_landmarks(image)
+
+        return image
+
+    def draw_landmarks(self, image):
+        pose_landmarks_list = self.results.pose_landmarks.landmark
+        annotated_image = np.copy(image)
+
+        # Draw the pose landmarks.
+        for landmark in pose_landmarks_list:
+            x = int(landmark.x * annotated_image.shape[1])
+            y = int(landmark.y * annotated_image.shape[0])
+            cv2.circle(annotated_image, (x, y), 5, (255, 0, 0), -1)
+
+        return annotated_image
+
+    def __del__(self):
+        self.pose.close()
--- main.py
+++ main.py
@@ -1,25 +1,15 @@
 import cv2
 import numpy as np
 import mediapipe as mp
-
-def draw_landmarks_on_image(rgb_image, detection_result):
-    pose_landmarks_list = detection_result.pose_landmarks.landmark
-    annotated_image = np.copy(rgb_image)
-
-    # Draw the pose landmarks.
-    for landmark in pose_landmarks_list:
-        x = int(landmark.x * annotated_image.shape[1])
-        y = int(landmark.y * annotated_image.shape[0])
-        cv2.circle(annotated_image, (x, y), 5, (255, 0, 0), -1)
-
-    return annotated_image
+import detect_pose
 
 def main():
+    print("Press 'q' to quit.")
     # Define a video capture object 
     vid = cv2.VideoCapture(0) 
 
     # Initialize MediaPipe Pose Landmark Model
-    mp_pose = mp.solutions.pose.Pose()
+    poseDetector = detect_pose.PoseDetector()
 
     while True: 
         # Capture the video frame by frame 
@@ -27,15 +17,8 @@ def main():
 
         if not ret:
             break
-
-        # Detect pose landmarks from the input image
-        results = mp_pose.process(frame)
-
-        # Process the detection result
-        if results.pose_landmarks:
-            annotated_image = draw_landmarks_on_image(frame, results)
-            # Display the resulting frame
-            cv2.imshow('MediaPipe Pose Detection', annotated_image)
+        
+        cv2.imshow('MediaPipe Pose Detection', poseDetector.find_pose(frame))
 
         # Check if the user pressed the 'q' key, if so quit.
         if cv2.waitKey(1) & 0xFF == ord('q'): 
