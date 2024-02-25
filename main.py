958be05f017d8d9c88672069c67cb68df48ef101
Antheablz <agblais@icloud.com>
2024-02-24 16:31:45 -0800

    flop

--- main.py
+++ main.py
@@ -1,41 +1,44 @@
-import cv2
-import numpy as np
 import mediapipe as mp
-import detect_pose
-import detect_hands
 
-def main():
-    # Define a video capture object 
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+PoseLandmarkerResult = mp.tasks.vision.PoseLandmarkerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+# Create a pose landmarker instance with the live stream mode:
+def print_result(result: PoseLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    print('pose landmarker result: {}'.format(result))
+
+options = PoseLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='pose_landmarker_full'),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+
+with PoseLandmarker.create_from_options(options) as landmarker:
+    pass
+  # The landmarker is initialized. Use it here.
+  # ...
+
+
+"""
+
     vid = cv2.VideoCapture(0) 
 
-    # Initialize MediaPipe Pose Landmark Model
-    poseDetector = detect_pose.PoseDetector()
-    handsDetector = detect_hands.HandDetector()
-
-    while True: 
-        # Capture the video frame by frame 
-        ret, frame = vid.read() 
+    while True:
+        ret, frame = vid.read()
 
         if not ret:
             break
-        
+
         frame = cv2.flip(frame, 1)
-        
-        frame = poseDetector.find_pose(frame)
-        frame = handsDetector.find_hands(frame)
-        
-        
+
+              
+
         cv2.imshow('MediaPipe Pose Detection', frame)
 
-        # Check if the user pressed the 'q' key, if so quit.
         if cv2.waitKey(1) & 0xFF == ord('q'): 
             break
 
-    # Release the video capture object
     vid.release()
-
-    # Close all OpenCV windows
-    cv2.destroyAllWindows()
-
-if __name__ == "__main__":
-    main()
+    cv2.destroyAllWindows()"""
