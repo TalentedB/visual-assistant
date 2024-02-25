eb766de389ea08e8d14885ca579eb38af2b6fbf5
Chris <81205126+TalentedB@users.noreply.github.com>
2024-02-24 20:46:25 -0800

    feat: Made selection representation work

--- mode_switch_gui.py
+++ mode_switch_gui.py
@@ -3,27 +3,67 @@ import threading
 
 
 class ModeSwitchGui(tk.Tk):
-    modes = ["Zoom", "Selection", "xyz"]
-    
+    modes = ["Zoom", "Selection", "Scroll", "Switch Window"]
+    labels = []
     
     def __init__(self):
         super().__init__()
-        self.title("Mode Switch")
-        self.geometry("700x400+-100+100")
-        self.overrideredirect(True)
-
         self.mode = 0
-
-        self.mode_label = tk.Label(self, textvariable=self.mode)
-        self.mode_label.pack(pady=20)
-        self.after(1000, self.destroy)
-        self.mainloop()
-
+        self.__createGui__()
         
-    def switchSelection(self):
+    def __createSelections__(self, selected=0):
+        fontSize = 20
+        
+        for i in range(len(self.modes)):
+            if i == selected:
+                self.__createLabel__(text=self.modes[i], x=self.height*i+100, y=self.height // 2 - fontSize, bg="red")
+            else:
+                self.__createLabel__(text=self.modes[i], x=self.height*i+100, y=self.height // 2 - fontSize)
+
+    def __destroyLabels__(self):
+        for label in self.labels:
+            label.destroy()
+        self.labels = []
+    
+    def __createLabel__(self, fontSize=20, x=0, y=0, text="", bg="white"):
+        text = tk.Label(self, text=text, font=("Arial", fontSize), bg=bg)
+        text.place(x=x, y=y)
+        self.labels.append(text)
+
+    def __createGui__(self, override=True):
+        
+        # Initial Setup
+        self.mode_label = tk.Label(self, textvariable=self.mode)
+        self.mode_label.place()
+        self.after(20000, self.destroy)
+        self.title("Mode Switch")
+        
+        self.width = 200 * len(self.modes)
+        self.height = 200
+        self.centerx = (self.winfo_screenwidth() // 2) - (self.width // 2)
+        self.centery = (self.winfo_screenheight() // 2) - (self.height // 2)
+        
+        self.geometry('{}x{}+{}+{}'.format(self.width, self.height, self.centerx, self.centery))
+        
+        self.overrideredirect(override)
+        
+        self.attributes('-alpha', 1)
+        
+        # Create the selection labels
+        self.__createSelections__()
+        
+        # Bind the tab key to switch the selection
+        self.bind("<Tab>", self.switchSelection)
+        
+        # Start the main loop
+        self.mainloop()
+        
+    def switchSelection(self, event):
         print("Switching selection")
         self.mode = (self.mode + 1) % len(self.modes)
-        self.mode_label.config(text=self.mode)
+        self.__destroyLabels__()
+        self.__createSelections__(self.mode)
+        
         self.update()
         
 
